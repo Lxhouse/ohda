12d8e5418fad7659b3d0c75d89bdf3a7c6c55b02
LxHouse <364403299@qq.com>
2024-07-29 00:50:09 +0800

    feat: 优化过后的版本

--- src/view/EditPage/Center/Canvas.tsx
+++ src/view/EditPage/Center/Canvas.tsx
@@ -1,62 +1,74 @@
-import { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { throttle } from 'lodash';
 import Cmp from './Cmp';
 import useEditStore, {
   addCmp,
   updateCanvasByAssembly,
 } from '@/store/editStore';
+
 function Canvas() {
   const canvas = useEditStore((state) => state.canvas);
+  const canvasRef = useRef<HTMLDivElement>(null);
+
   const onDrop = useCallback((e: React.DragEvent) => {
     e.preventDefault();
-    /** 获取拖拽的组件信息 */
     const dragInfo = e.dataTransfer.getData('drag-cmp');
     if (!dragInfo) return;
+
     const dragCmp = JSON.parse(dragInfo || '{}');
-    /** 获取它放在画布上的位置，计算相对于画布的位置 */
-    const endX = e.pageX;
-    const endY = e.pageY;
-    const canvas = document.querySelector('#canvas');
-    const { top = 0, left = 0 } = canvas?.getBoundingClientRect() || {};
-    const disX = endX - left;
-    const disY = endY - top;
+    const { top = 0, left = 0 } =
+      canvasRef.current?.getBoundingClientRect() || {};
+    const disX = e.pageX - left;
+    const disY = e.pageY - top;
+
     dragCmp.style.left = disX;
     dragCmp.style.top = disY;
-    /** 值传给store */
+
     addCmp(dragCmp);
   }, []);
+
   const onDragOver = useCallback((event: React.DragEvent) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
   }, []);
-  const handelMove = (e: React.MouseEvent) => {
+
+  const handleMove = useCallback((e: React.MouseEvent) => {
+    e.preventDefault();
+
     let startX = e.pageX;
     let startY = e.pageY;
-    const move = throttle((e1: React.MouseEvent) => {
+
+    const move = throttle((e1: MouseEvent) => {
       const disX = e1.pageX - startX;
       const disY = e1.pageY - startY;
+
       updateCanvasByAssembly({ top: disY, left: disX });
+
       startX = e1.pageX;
       startY = e1.pageY;
-    });
+    }, 50); // Adjust throttle delay as needed
+
     const up = () => {
       document.removeEventListener('mousemove', move);
       document.removeEventListener('mouseup', up);
     };
+
     document.addEventListener('mousemove', move);
     document.addEventListener('mouseup', up);
-  };
+  }, []);
+
   return (
     <div
       id="canvas"
-      className="relative h-full overflow-auto "
+      className="relative h-full overflow-auto"
       style={canvas.style}
+      ref={canvasRef}
       onDrop={onDrop}
       onDragOver={onDragOver}
-      onMouseDown={handelMove}
+      onMouseDown={handleMove}
     >
       {canvas.cmps.map((cmp, i) => (
-        <Cmp cmp={cmp} index={i} />
+        <Cmp key={i} cmp={cmp} index={i} />
       ))}
     </div>
   );
